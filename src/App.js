2925893bfbcf64b106c2bbcfbb173c7dc55b41c1
mecozma <mecozma@gmail.com>
2018-12-11 20:18:32 +0000

    Understanding and using the State property

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Eduard', age: 33},
+      {name: 'Andrei', age: 31},
+      {name: 'Carla', age: 29}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi! I'm a React App!</h1>
-        <Person name="Eduard" age="33" />
-        <Person name="Andrei" age="31"/>
-        <Person name="Carla" age="29">My Hobbies: JavaScript</Person >
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >My Hobbies: JavaScript</Person >
       </div>
     );
   }
