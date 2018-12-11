a539962a27e03459a00bfd9f8f7ec5f7b3d661e8
mecozma <mecozma@gmail.com>
2018-12-11 20:47:33 +0000

    Handling events with Metods

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    console.log('I was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi! I'm a React App!</h1>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >My Hobbies: JavaScript</Person >
