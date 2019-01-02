d1a18af9ef5d6f143eba5e10742130bab8a928fb
mecozma <mecozma@gmail.com>
2019-01-02 12:03:52 +0000

    Flexible lists

--- src/App.js
+++ src/App.js
@@ -13,13 +13,21 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        {name: event.target.value, age: 33},
-        {name: 'Andrei', age: 31},
-        {name: 'Carla', age: 29}
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons
     })
   }
 
@@ -59,7 +67,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
             })}
           </div>
       );
