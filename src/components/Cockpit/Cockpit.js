6055bf71d22c96641814ab5f9018ad608a22afe3
mecozma <mecozma@gmail.com>
2019-01-30 17:50:01 +0000

    Returning Adjacent Elements

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Auxiliary from '../../hoc/Auxiliary';
 
 const cockpit = (props) => {
   // Changing the <p> styless by adding classes dynamically
   let assignedClasses = [];
 
-  let btnClasses = '';
+  let btnClasses = classes.Button;
   if (props.showPersons) {
-    btnClasses = classes.Red;
+    btnClasses = [classes.Button ,classes.Red].join(' ');
   }
 
   if (props.persons.length <= 2) {
@@ -17,13 +18,13 @@ const cockpit = (props) => {
     assignedClasses.push(classes.bold);
   }
   return (
-    <div className={classes.Cockpit}>
+    <Auxiliary>
           <h1>Hi! I'm a React App!</h1>
           <p className={assignedClasses.join(' ')}>This is a paragraph</p>
           <button
             className={btnClasses}
             onClick={props.clicked}>Toggle Persons List</button> 
-        </div>
+    </Auxiliary>
   )
 }
 
